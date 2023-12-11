8636a5b7f5672c5bba12dd38e624540f6229e2ea
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-12-11 16:17:44 -0300

    Initial commit

---
+++ type-number.js
@@ -0,0 +1,25 @@
+// Tipo Number
+
+const meuNumero = 3
+
+const promeiroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = promeiroNumero * segundoNumero
+
+console.log(operacaoMatematica)
+
+//Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3
+
+const pontoFlutuante = .5
+
+const novaOperação = promeiroNumero / numeroPontoFlutuante
+console.log(novaOperação)
+
+//NaN -> Not a Number
+
+const alura = 'Alura'
+
+console.log(alura * promeiroNumero)
