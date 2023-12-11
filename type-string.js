43d1d0905384ec0115d4bc6e12a2c86227db8d3f
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-12-11 16:29:08 -0300

    Create type-string.js

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!"
+const texto2 = 'Olá, mundo! 2'
+
+const senha = "senhaSuperSegura123456!"
+const stringDeNumeros = '34567'
+
+const citacao = 'Meu nome é: '
+const meuNome = 'Leonardo'
+
+// Concatenação (+)
+console.log(citacao + meuNome)
+
+//Template String ou Template Literal
+const TemplateString = `O leo disse "${citacao}"`
+//console.log(TemplateString)
