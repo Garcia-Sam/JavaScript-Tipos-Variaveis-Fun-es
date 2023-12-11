849ee20b1fd465040e227c69017de6bad45bcb73
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-12-11 16:40:16 -0300

    Create type-boolean.js

---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+const primeiroNumero = 5
+const segundoNumero = 10
+const terceiroNumero = 5
+
+let cadastroAtivado = true
+
+//console.log(primeiroNumero === segundoNumero)
+//console.log(primeiroNumero === terceiroNumero)
+
+const texto1 = "Alura"
+const texto2 = "alura"
+
+console.log(texto1 === texto2)
