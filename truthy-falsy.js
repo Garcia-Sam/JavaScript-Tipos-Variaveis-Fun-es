4918b5f1dd29de8c48b624aa74b8142cda80a406
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-12-12 17:11:33 -0300

    Create truthy-flasy.js

---
+++ truthy-falsy.js
@@ -0,0 +1,27 @@
+//boolean
+const usuarioLogado = true 
+const conotaPaga = false
+
+// truthy ou falsy - não são booleanos, mas se comportam como
+
+// 0 ==> false
+// 1 ==> true
+
+//console.log(0 == false)   // ==> true
+//console.log('' == false) // ==> true
+
+//console.log(1 == true) // ==> true
+
+// undefined - variável declarada mas sem valor
+
+// null ==> vazio ou nada
+
+let minhaVar
+let varNULL = null
+console.log(typeof minhaVar)
+console.log(typeof varNULL)
+
+let numero = 3
+let texto = 'Alura'
+//console.log(typeof numero)
+//console.log(typeof texto)
