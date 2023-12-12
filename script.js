72ea05be0973a43d42202cdced3257e66a959e89
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-12-12 16:57:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 //var - pode ser declarada antes de ser utilizada 
 
-var altura = 5
-var comprimento = 7
+// var altura = 5
+// var comprimento = 7
 
-area = altura * comprimento
+// area = altura * comprimento
+// console.log(area)
+// var area
+
+//let -
+// let forma = "retângulo"
+// let altura = 5
+// let comprimento = 7
+// let area
+
+// if (forma === "retângulo") {
+//     area = altura * comprimento
+// } else {
+//     area = (altura * comprimento) / 2
+// }
+// console.log(area)
+
+//const 
+const forma = 'Triângulo'
+const altura = 5
+const comprimento = 7
+let area;
+
+if (forma === 'Quadrado') {
+    area = altura * comprimento
+} else {
+    area = (altura * comprimento) / 2
+}
+
 console.log(area)
-var area
-
-//let
-
-let
