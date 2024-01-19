997c7699f38505abb17e0b16fedb924ee2b93094
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2024-01-19 17:11:58 -0300

    Create erros.js

---
+++ erros.js
@@ -0,0 +1,8 @@
+// const numero; // SyntaxError: Missing initializer in const declaration - falta inicializador na declaração const -> const numero = 0;
+
+const numero = 1;
+// console.log(numero; // SyntaxError: missing ) after argument list
+
+//const minhaVar = "Oi"
+console.log(minhaVar); // ReferenceError: minhaVar is not defined
+ 
