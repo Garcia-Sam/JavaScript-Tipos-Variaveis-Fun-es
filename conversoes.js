5da0bab3cea34d864ba3d2da1539183baf4f5a24
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2024-01-19 15:39:38 -0300

    Create conversoes.js

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// tipos de dados
+//booleanos
+
+// Conversão Implícita 
+const numero = 456;
+const numeroString = "456";
+
+// console.log(numero == numeroString) // == -> faz comparação por valor
+
+// console.log(numero + numeroString)
+
+console.log(numero + Number(numeroString))
+
+
+// Conversão Explícita
+// Number()
+// String()
