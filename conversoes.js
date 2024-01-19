8a1303ba13aaf1691244fb379172e36b58c3b660
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2024-01-19 15:53:04 -0300

    Update conversoes.js

--- conversoes.js
+++ conversoes.js
@@ -9,9 +9,22 @@ const numeroString = "456";
 
 // console.log(numero + numeroString)
 
-console.log(numero + Number(numeroString))
+//console.log(numero + Number(numeroString))
 
 
 // Conversão Explícita
 // Number()
 // String()
+
+// var respostaDeTudo = 42
+// let idade = 29
+// const pi = 3.14
+
+// {
+//     var respostaDeTudo = 3.14
+//     let idade = 42
+//     const pi = 29
+//     console.log(respostaDeTudo, idade, pi)
+
+// }
+// console.log(respostaDeTudo, idade, pi)
